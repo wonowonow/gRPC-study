bed6042014ff49cc343c01d55bdf11939aa54bac
wonowonow <123870616+wonowonow@users.noreply.github.com>
2025-04-12 23:41:35 +0900

    first commit

---
+++ src/test/java/study/grpc/GRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package study.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
